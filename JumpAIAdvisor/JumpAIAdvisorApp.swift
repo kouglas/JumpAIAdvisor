9354608ea643aac1aaa7d8928d5d0173c42216a3
Kari <kouglas2@gmail.com>
2025-05-10 16:12:54 -0500

    Initial Commit

---
+++ JumpAIAdvisor/JumpAIAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpAIAdvisorApp.swift
+//  JumpAIAdvisor
+//
+//  Created by Kari Douglas on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct JumpAIAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
